97fa2d42773a0523da18b2462832406fcbe7e9f5
Sraddheya <ggsraddheya@gmail.com>
2021-03-20 15:33:59 +0000

    Edited metricGraph by calling euclideanGraph

--- tests.py
+++ tests.py
@@ -28,37 +28,3 @@ def metricGraph(numNodes, maxDist):
         for v in range(u+1, numNodes):
             writer.write(str(u) + " " + str(v) + " " + str(g.dist[u][v]) + "\n")
     writer.close()
-
-#w(u, v) ≤ w(u, x) + w(x, v)
-#def metricGraph(numNodes, maxDist):
-#    writer = open('metricGraph', 'w', encoding='utf-8')
-#    writer.truncate(0)
-#    dist = [[None for i in range(numNodes)] for j in range(numNodes)]
-#    distance = random.randint(1,maxDist)
-#    writeMetric(writer, dist, distance, 0, 1)
-#    for u in range(2,numNodes):
-#        distance = random.randint(1,maxDist)
-#        writeMetric(writer, dist, distance, u, u-1)
-#        for v in range(u-1):
-#            distance = math.floor(math.sqrt(dist[u][u-1]*dist[u][u-1] + dist[u-1][v]*dist[v][u-1]))
-#            if distance > maxDist:
-#                distance = maxDist
-#            writeMetric(writer, dist, distance, u, v)
-#    writer.close()
-#    isTriangle(dist, numNodes)
-
-#def writeMetric(writer, dist, distance, u, v):
-#    dist[u][v] = distance
-#    dist[v][u] = distance
-#    writer.write(str(u) + " " + str(v) + " " + str(distance) + "\n")
-
-def isTriangle(dist, numNodes):
-    for u in range(numNodes):
-        for v in range(numNodes):
-            if u!=v:
-                for x in range(numNodes):
-                    if x!= u and x!=v:
-                        if dist[u][v]>dist[u][x]+dist[x][v]:
-                            print(False)
-                            print(str(u) + " " + str(x) + " " + str(v) + " " + str(dist[u][v]) + " " + str(dist[u][x]) + " " + str(dist[x][v]))
-    print(True)
