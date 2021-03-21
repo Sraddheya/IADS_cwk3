b7a6e07a5629261901a908012bbb64a4aaf96b56
Sraddheya <ggsraddheya@gmail.com>
2021-03-21 09:52:11 +0000

    Edited to comapre against optimmum)

--- tests.py
+++ tests.py
@@ -30,50 +30,59 @@ def metricGraph(numNodes, maxDist):
             writer.write(str(u) + " " + str(v) + " " + str(g.dist[u][v]) + "\n")
     writer.close()
 
-def printTest(g, n, filename):
+def printTest(g, n, opt, filename):
     #Initial
     print("Initial: " + str(g.perm) + " " + str(g.tourValue()))
 
     #swapHeuristic
     time = timeit.timeit('g.swapHeuristic(' + str(g.n)+ ')', number = 1, setup = 'import graph\ng = graph.Graph(' + str(n) + ', "' + filename + '")')
     g.swapHeuristic(g.n)
-    print("swapHeuristic: " + str(g.perm) + " " + str(g.tourValue()) + " " + str(time))
+    swapTour = g.tourValue()
+    print("swapHeuristic: " + str(g.perm) + " " + str(swapTour) + " " + str(time))
 
     #TwoOptHeuristic
     time = timeit.timeit('g.TwoOptHeuristic(' + str(g.n)+ ')', number = 1, setup = 'import graph\ng = graph.Graph(' + str(n) + ', "' + filename + '")')
     g.initPerm()
     g.TwoOptHeuristic(g.n)
-    print("TwoOptHeuristic: " + str(g.perm) + " " + str(g.tourValue()) + " " + str(time))
+    twoOptTour = g.tourValue()
+    print("TwoOptHeuristic: " + str(g.perm) + " " + str(twoOptTour) + " " + str(time))
 
     #Greedy
     time = timeit.timeit('g.Greedy()', number = 1, setup = 'import graph\ng = graph.Graph(' + str(n) + ', "' + filename + '")')
     g.initPerm()
     g.Greedy()
-    print("Greedy: " + str(g.perm) + " " + str(g.tourValue()) + " " + str(time))
+    greedyTour = g.tourValue()
+    print("Greedy: " + str(g.perm) + " " + str(greedyTour) + " " + str(time))
 
     if filename != 'nonMetricGraph':
         #Prim
         time = timeit.timeit('g.Prim()', number = 1, setup = 'import graph\ng = graph.Graph(' + str(n) + ', "' + filename + '")')
         g.initPerm
         g.Prim()
-        print("Prim: " + str(g.perm) + " " + str(g.tourValue()) + " " + str(time))
+        primTour = g.tourValue()
+        print("Prim: " + str(g.perm) + " " + str(primTour) + " " + str(time))
 
-def euclideanTest(filename):
+    if opt > 0:
+        approx = [swapTour, twoOptTour, greedyTour, primTour]
+        approx = [round(i/opt, 2) for i in approx]
+        print("How close to optimim: " + str(opt) + "? " + str(approx))
+
+def euclideanTest(filename, opt):
     if filename=="euclideanGraph":
         euclideanGraph(random.randint(3,20), random.randint(100,1000), random.randint(100,1000))
     g = graph.Graph(-1, filename)
-    printTest(g, -1, filename)
+    printTest(g, -1, opt, filename)
 
 def nonMetricTest():
     numNodes = random.randint(3,20)
     filename = 'nonMetricGraph'
     nonMetricGraph(numNodes, random.randint(1,100))
     g = graph.Graph(numNodes, filename)
-    printTest(g, numNodes, filename)
+    printTest(g, numNodes, -1, filename)
 
 def metricTest():
     numNodes = random.randint(3,20)
     filename = 'metricGraph'
     metricGraph(numNodes, random.randint(1,100))
     g = graph.Graph(numNodes, filename)
-    printTest(g, numNodes, filename)
+    printTest(g, numNodes, -1, filename)
